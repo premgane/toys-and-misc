3af1d86733df5bb806e4d044c5b819fe59797096
premgane <ganeprem@gmail.com>
2021-04-21 22:52:03 -0400

    Some toys

--- python_syntax_refresher.py
+++ python_syntax_refresher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-print("{0} be nimble, {1} be quick, {0} sit on a candlestick".format("Jack", "Jack Flash"))
+print("{0} be nimble, {1} be quick, {0} sit on a candlestick"
+      .format("Jack", "Jack Flash"))
 
 li = []
 
@@ -21,7 +22,7 @@ print(li.extend(li))
 print(li)
 print(li + li)
 
-print((2,3,'hello'))
+print((2, 3, 'hello'))
 print((5,))
 
 d, e, f = 5, 6, 7
@@ -36,7 +37,7 @@ mapping[5] = 6
 print(mapping.keys())
 list(mapping.keys())
 
-mapping.update({7:8})
+mapping.update({7: 8})
 
 print(list(mapping))
 
@@ -56,7 +57,7 @@ elif 6 in my_set:
 
 for element in li + [9, 0, 0, 0, 43]:
     print(element, end=" | ")
-    
+
 print()
 
 total = 0
@@ -69,12 +70,14 @@ with open("README.md") as f:
     for line in f:
         print(line[0])
 
+
 def print_nums(x, y):
     '''
     Print two numbers and return them but swapped
     '''
     print("{a} and {b}".format(a=x, b=y))
     return y, x
+
 
 wow, hey = print_nums(6, 5)
 print(wow)
@@ -85,4 +88,4 @@ print(list([x for x in range(99) if x % 30 is 0]))
 # Map an anonymous function on a list
 print(list(map(lambda x: x ** 4, [6, 7, 8, 9])))
 
-print({x : x ** 8 for x in range(9)})
+print({x: x ** 8 for x in range(9)})
